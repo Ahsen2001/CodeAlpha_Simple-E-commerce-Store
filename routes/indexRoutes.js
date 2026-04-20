d92b6bec827280e88fa0919d61b813ba4362afcb
Ahsen <uahsens1@gmail.com>
2026-04-20 11:29:59 +0530

    chore: remove unused file and associated references

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const productController = require('../controllers/productController');
 
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Modern E-Commerce Store' });
-});
+// Homepage with products
+router.get('/', productController.getHomepage);
+
+// We'll prepare the product details route for Phase 3
+router.get('/product/:id', productController.getProductDetails);
 
 module.exports = router;
